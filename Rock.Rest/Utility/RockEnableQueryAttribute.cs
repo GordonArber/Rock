ef6f48cf8b9f21c8b9e3347e8c45f6b7993d095a
Mike Peterson <mike@sparkdevnetwork.org>
2022-10-05 14:59:10 -0700

    - Proposal for backwards support for ODataV3 filters

--- Rock.Rest/Utility/RockEnableQueryAttribute.cs
+++ Rock.Rest/Utility/RockEnableQueryAttribute.cs
@@ -17,8 +17,12 @@
 using System;
 using System.Linq;
 using System.Net.Http;
-using System.Web.Http.Filters;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
 using Microsoft.AspNet.OData;
 using Microsoft.AspNet.OData.Query;
 
@@ -36,9 +40,7 @@ namespace Rock.Rest
         /// <param name="elementClrType">The CLR type to retrieve a model for.</param>
         /// <param name="request">The request message to retrieve a model for.</param>
         /// <param name="actionDescriptor">The action descriptor for the action being queried on.</param>
-        /// <returns>
-        /// The EDM model for the given type and request.
-        /// </returns>
+        /// <returns>The EDM model for the given type and request.</returns>
         public override Microsoft.OData.Edm.IEdmModel GetModel( Type elementClrType, System.Net.Http.HttpRequestMessage request, System.Web.Http.Controllers.HttpActionDescriptor actionDescriptor )
         {
             // use the EdmModel that we already created in WebApiConfig (so that we don't have problems with OData4 Open Types)
@@ -47,29 +49,141 @@ namespace Rock.Rest
             return ourModel;
         }
 
+        /// <summary>
+        /// Applies the query to the given entity based on incoming query from uri and query settings.
+        /// </summary>
+        /// <param name="entity">The original entity from the response message.</param>
+        /// <param name="queryOptions">The <see cref="T:Microsoft.AspNet.OData.Query.ODataQueryOptions" /> instance constructed based on the incoming request.</param>
+        /// <returns>The new entity after the $select and $expand query has been applied to.</returns>
         public override object ApplyQuery( object entity, ODataQueryOptions queryOptions )
         {
-            return base.ApplyQuery( entity, queryOptions );
+            return base.ApplyQuery( entity, EnsureV4ODataQueryOptions( queryOptions ) );
         }
 
+        /// <summary>
+        /// Applies the query to the given IQueryable based on incoming query from uri and query settings. By default,
+        /// the implementation supports $top, $skip, $orderby and $filter. Override this method to perform additional
+        /// query composition of the query.
+        /// </summary>
+        /// <param name="queryable">The original queryable instance from the response message.</param>
+        /// <param name="queryOptions">The <see cref="T:Microsoft.AspNet.OData.Query.ODataQueryOptions" /> instance constructed based on the incoming request.</param>
+        /// <returns>IQueryable.</returns>
         public override IQueryable ApplyQuery( IQueryable queryable, ODataQueryOptions queryOptions )
         {
-            return base.ApplyQuery( queryable, queryOptions );
+            return base.ApplyQuery( queryable, EnsureV4ODataQueryOptions( queryOptions ) );
         }
 
+        /// <summary>
+        /// Validates the OData query in the incoming request. By default, the implementation throws an exception if
+        /// the query contains unsupported query parameters. Override this method to perform additional validation of
+        /// the query.
+        /// </summary>
+        /// <param name="request">The incoming request.</param>
+        /// <param name="queryOptions">The <see cref="T:Microsoft.AspNet.OData.Query.ODataQueryOptions" /> instance constructed based on the incoming request.</param>
         public override void ValidateQuery( HttpRequestMessage request, ODataQueryOptions queryOptions )
         {
-            return;
+            base.ValidateQuery( request, EnsureV4ODataQueryOptions( queryOptions ) );
         }
 
-        public override void OnActionExecuted( HttpActionExecutedContext actionExecutedContext )
+        /// <summary>
+        /// Ensures the v4 o data query options.
+        /// </summary>
+        /// <param name="queryOptions">The query options.</param>
+        /// <returns>ODataQueryOptions.</returns>
+        private ODataQueryOptions EnsureV4ODataQueryOptions( ODataQueryOptions queryOptions )
         {
-            base.OnActionExecuted( actionExecutedContext );
+            if ( InferIfRequestUriHasV3Filters( queryOptions ) )
+            {
+                return ConvertOData3FiltersToODataV4( queryOptions );
+            }
+            else
+            {
+                return queryOptions;
+            }
         }
 
-        public override void OnActionExecuting( HttpActionContext actionContext )
+        // Sometimes clients will not specify dataserviceversion or maxdataserviceversion
+        // While this is required in the spec, we can quietly check a couple giveaways that the request URI is v3
+        /// <summary>
+        /// Infers if request URI has v3 filters.
+        /// </summary>
+        /// <param name="queryOptions">The query options.</param>
+        /// <returns><c>true</c> if XXXX, <c>false</c> otherwise.</returns>
+        private bool InferIfRequestUriHasV3Filters( ODataQueryOptions queryOptions )
         {
-            base.OnActionExecuting( actionContext );
+            var rawFilter = queryOptions?.Filter?.RawValue;
+            if ( rawFilter.IsNullOrWhiteSpace() )
+            {
+                return false;
+            }
+
+            var isV3DateTimeFilter = _dateTimeFilterCapture.IsMatch( rawFilter );
+            var isV3GuidFilter = _guidFilterCapture.IsMatch( rawFilter );
+            return isV3DateTimeFilter || isV3GuidFilter;
+        }
+
+        /// <summary>
+        /// The date time filter capture
+        /// </summary>
+        private readonly Regex _dateTimeFilterCapture = new Regex( @"datetime\'(\S*)\'", RegexOptions.Compiled );
+        /// <summary>
+        /// The unique identifier filter capture
+        /// </summary>
+        private readonly Regex _guidFilterCapture = new Regex( @"guid\'([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12})\'", RegexOptions.Compiled );
+
+        /// <summary>
+        /// Converts the o data3 filters to o data v4.
+        /// </summary>
+        /// <param name="queryOptions">The query options.</param>
+        /// <returns>ODataQueryOptions.</returns>
+        private ODataQueryOptions ConvertOData3FiltersToODataV4( ODataQueryOptions queryOptions )
+        {
+            var originalUrl = queryOptions.Request.RequestUri.OriginalString;
+            var rawFilter = queryOptions?.Filter?.RawValue;
+            var dateTimeMatches = _dateTimeFilterCapture.Matches( rawFilter );
+            var updatedUrl = originalUrl;
+
+            foreach ( Match match in dateTimeMatches )
+            {
+                if ( match.Groups.Count == 2 )
+                {
+                    var v3Filter = match.Groups[0].Value;
+                    var capture = match.Groups[1].Value;
+
+                    var replace = System.Net.WebUtility.UrlEncode( v3Filter );
+                    var replaceWith = System.Net.WebUtility.UrlEncode( capture );
+                    updatedUrl = updatedUrl.Replace( replace, replaceWith );
+                }
+            }
+
+            var guidMatches = _guidFilterCapture.Matches( rawFilter );
+            foreach ( Match match in guidMatches )
+            {
+                if ( match.Groups.Count == 2 )
+                {
+                    var v3Filter = match.Groups[0].Value;
+                    var capture = match.Groups[1].Value;
+
+                    var replace = System.Net.WebUtility.UrlEncode( v3Filter );
+                    var replaceWith = System.Net.WebUtility.UrlEncode( capture );
+                    updatedUrl = updatedUrl.Replace( replace, replaceWith );
+                }
+            }
+            
+            var convertedRequest = new HttpRequestMessage( queryOptions.Request.Method, updatedUrl );
+            foreach( var origProperty in queryOptions.Request.Properties )
+            {
+                convertedRequest.Properties.Add( origProperty.Key, origProperty.Value );
+            }
+
+            foreach ( var origProperty in queryOptions.Request.Headers )
+            {
+                convertedRequest.Headers.Add( origProperty.Key, origProperty.Value );
+            }
+
+            var convertedODataQueryOptions = new ODataQueryOptions( queryOptions.Context, convertedRequest );
+
+            return convertedODataQueryOptions;
         }
     }
 }
